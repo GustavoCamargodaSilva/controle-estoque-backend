7f90390e0856be23ffa51b1f7cee25538efdb3a3
Gustavo Camargo <104240187+GustavoCamargodaSilva@users.noreply.github.com>
2024-02-11 22:05:46 -0300

    criaçao classe produto

---
+++ estoque/src/main/java/com/domain/estoque/entities/Produto.java
@@ -0,0 +1,11 @@
+package com.domain.estoque.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity(name = "produto")
+@Table(name = "tb_produto")
+public class Produto {
+
+    
+}
